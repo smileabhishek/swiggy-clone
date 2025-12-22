f6893c24c2f3aedf5a56201cefe92c2d66c45152
Abhishek Anand <abhishekanand1138@gmail.com>
2025-12-23 01:44:53 +0530

    feat: add footer with navigation

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+export default function Footer() {
+    return (
+        <footer>
+            <div className="footer-content">
+                <p>© 2024 Swiggy Clone. All rights reserved.</p>
+            </div>
+            <div className="footer-nav">
+                <ul>
+                    <li>
+                        <a href="#">About Us</a>
+                    </li>
+                    <li>
+                        <a href="#">Careers</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    );
+}
