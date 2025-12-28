e08faee9dd1d6233f00ff38a78fa38433e4a444d
Abhishek Anand <abhishekanand1138@gmail.com>
2025-12-29 02:03:35 +0530

    feat: add dynamic items from restaurant list to grid

--- src/components/RestaurantGrid/RestauratnGrid.jsx
+++ src/components/RestaurantGrid/RestauratnGrid.jsx
@@ -1,17 +1,20 @@
 import RestaurantCard from "../RestaurantCard/RestaurantCard";
 import styles from "./RestaurantGrid.module.css";
+import restaurantsList from "../../assets/restaurantData.json";
 const RestaurantGrid = () => {
+    const restaurantList =
+        restaurantsList?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+            ?.restaurants;
     return (
         <div className="restaurant-grid">
             <h2>Restaurants with online food delivery in Bangalore</h2>
             <div className={styles.restaurantGridContainer}>
-                <RestaurantCard className="restaurant-card-1" />
-                <RestaurantCard className="restaurant-card-2" />
-                <RestaurantCard className="restaurant-card-3" />
-                <RestaurantCard className="restaurant-card-3" />
-                <RestaurantCard className="restaurant-card-3" />
-                <RestaurantCard className="restaurant-card-3" />
-                <RestaurantCard className="restaurant-card-3" />
+                {restaurantList.map((restaurant) => (
+                    <RestaurantCard
+                        key={restaurant.info.id}
+                        restaurantData={restaurant}
+                    />
+                ))}
             </div>
         </div>
     );
