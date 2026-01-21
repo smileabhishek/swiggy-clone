ca9acddf385e88bb3ab0aa8db64230da0d1d0a1f
Abhishek Anand <abhishekanand1138@gmail.com>
2026-01-22 02:28:09 +0530

    refactor: display new api data

--- src/components/RestaurantGrid/RestauratnGrid.jsx
+++ src/components/RestaurantGrid/RestauratnGrid.jsx
@@ -15,10 +15,11 @@ const RestaurantGrid = () => {
             const restaurnatList =
                 data?.data?.data?.cards[1]?.card?.card?.gridElements
                     ?.infoWithStyle?.restaurants;
-            setRestaurants([restaurnatList].slice(0, 8)); // limiting to 8 restaurants
+            setRestaurants(...[restaurnatList].slice(0, 8)); // limiting to 8 restaurants
         }
         fetchData();
     }, []);
+    console.log("restaurants:", restaurants);
     // static data from json file
     // const [restaurants, setRestaurants] = useState(restaurantsList?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
     //                     ?.restaurants);
--- src/pages/RestaurantPage.jsx
+++ src/pages/RestaurantPage.jsx
@@ -1,5 +1,35 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { MENU_API_URL } from "../utils/constants";
+
 const RestaurantPage = () => {
-    return <div>Restaurant Page</div>;
+    const { id: restaurantId } = useParams();
+    const [restaurantData, setRestaurantData] = useState(null);
+
+    useEffect(() => {
+        if (!restaurantId) return;
+
+        const fetchMenu = async () => {
+            try {
+                const res = await fetch(`${MENU_API_URL}${restaurantId}`);
+                if (!res.ok) throw new Error(`HTTP ${res.status}`);
+                const data = await res.json();
+                setRestaurantData(data.data.cards[2].card.card.info);
+            } catch (error) {
+                console.error("Error fetching menu:", error);
+            }
+        };
+
+        fetchMenu();
+    }, [restaurantId]);
+    if (!restaurantData) {
+        return <div>Loading...</div>;
+    }
+    return (
+        <div>
+            {restaurantData.name} - {restaurantId} Page
+        </div>
+    );
 };
 
 export default RestaurantPage;
