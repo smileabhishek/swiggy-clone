395297679503e7ce8a68c61a41e84c21276dd342
Abhishek Anand <abhishekanand1138@gmail.com>
2026-01-22 02:15:32 +0530

    "feat: updated anchor tags to Link tags

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,28 @@
 import SwiggyIcon from "../icons/SwiggyIcon";
 import { NAV_ITEMS } from "../../config/navigation";
 import styles from "./header.module.css";
-
+import { Link } from "react-router-dom";
 function Header({ className }) {
     return (
         <header className={`${styles.header} ${className ?? ""}`}>
             <nav className={styles.navBar}>
                 <div className={styles.logoLink}>
-                    <a href="/">
+                    <Link to="/">
                         <SwiggyIcon size={50} />
-                    </a>
+                    </Link>
                 </div>
 
                 <ul className={styles.navList}>
                     {NAV_ITEMS.map(({ id, label, Icon, href }) => (
                         <li key={id} className={styles.navItems}>
-                            <a href={href} className={styles.navItemContainer}>
+                            <Link to={href} className={styles.navItemContainer}>
                                 <span>
                                     <Icon size={24} aria-hidden="true" />
                                 </span>
                                 <span className={styles.navItemsText}>
                                     {label}
                                 </span>
-                            </a>
+                            </Link>
                         </li>
                     ))}
                 </ul>
